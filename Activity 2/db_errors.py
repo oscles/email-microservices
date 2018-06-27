ff09bdfbc9bd7c19e865a9c7f63650474da28139
oscles <osnaiderluis94@gmail.com>
2018-06-27 12:00:55 -0500

    new structure

---
+++ Activity 2/db_errors.py
@@ -0,0 +1,40 @@
+ERRORS_LIST = [
+    {
+        'code': 1020,
+        'type': 'LAN Connection Error	LAN communication failure.',
+        'body': '''Please check the network settings for your PC and RTR-500W, such as the IP address. (See: Q&A about Settings),
+                If the network settings are correct but this error occurs frequently, the FTP/E-mail transmission failure 
+                may be repeatedly occurring. 
+                Try turning off the transmission of data and warnings via FTP/E-mail and see if that stops the error.'''
+    },
+    {
+        'code': 1022,
+        'type': 'Failure to open USB',
+        'body': 'Reinsert the USB cable.'
+    },
+    {
+        'code': 1028,
+        'type': 'Reception Timeout Error',
+        'body': '''Communication with the device failed. Please wait and try again. 
+                If this error occurs frequently, the FTP/E-mail transmission failure may be repeatedly occurring.
+                Try turning off the transmission of data and warnings via FTP/E-mail and see if that stops the error.'''
+    },
+    {
+        'code': 20006,
+        'type': 'This version of the application is old.',
+        'body': 'Update the application to the latest version.'
+    },
+    {
+        'code': 40001,
+        'type': 'La comunicación inalámbrica está ocupada.',
+        'body': '''Se está procesando otra sesión de comunicación inalámbrica. 
+                Espere y vuelva a intentarlo.'''
+    },
+    {
+        'code': 40002,
+        'type': 'Wireless Communication Failure (Remote Unit).',
+        'body': '''Cambie las posiciones del dispositivo base, los 
+                repetidores o las unidades remotas y vuelva a intentarlo.'''
+    },
+
+]
---
+++ Activity 2/worker_write_logs.py
@@ -0,0 +1,39 @@
+import json
+
+import pika
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters('localhost')
+)
+
+channel = connection.channel()
+
+channel.exchange_declare(
+    exchange='logs',
+    exchange_type='fanout',
+)
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(
+    exchange='logs',
+    queue=queue_name
+)
+
+print(f'[*] Starting worker write logs with queue {queue_name}')
+
+
+def callback(channel, method, properties, body):
+    with open('logs.log', 'a') as csv:
+        data = json.loads(body)['body']
+        message = f"[{data.get('code')}] {data.get('type')}\n"
+        csv.writelines(message)
+    csv.close()
+    print(f'[*] Message for broker write logs {queue_name}.')
+    print(f'A new line was created in the logs.log file --- {message}.')
+
+
+channel.basic_consume(callback, queue=queue_name, no_ack=False)
+channel.start_consuming()
